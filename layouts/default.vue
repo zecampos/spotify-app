d88036edd5dbf1ba5f482097d3a7cbc41a795e47
zecampos <joseguilhermesantoscampos@gmail.com>
2018-11-26 23:06:29 -0200

    instalando vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+<v-app>
+  <v-content>
+    <v-container fluid>
+      <Header></Header>
+      <nuxt/>
+    </v-container>
+  </v-content>
+</v-app>
+ 
+</template>
+<script>
+import Header from '~/components/Header'
+export default {
+  components :{
+    Header
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <v-btn color="success">Success</v-btn>
+    <v-btn color="error">Error</v-btn>
+    <v-btn color="warning">Warning</v-btn>
+    <v-btn color="info">Info</v-btn>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+};
+</script>
+
+<style>
+</style>
