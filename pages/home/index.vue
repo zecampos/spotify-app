246d81dfd1ec31d2bf57ac6bdd5e7e7c41635e8b
zecampos <joseguilhermesantoscampos@gmail.com>
2018-12-04 13:15:05 -0200

    delete toaster3

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,91 +1,51 @@
 <template>
-<div>
-  <v-layout fill-height justify-center align-center row wrap>
-    
-    <v-flex xs12 sm12 md12 lg6 xl6>
-      <!--<pulse-loader style="text-align:center" :loading="loading" color="#4caf50" size="30px"></pulse-loader>-->
-      <v-expansion-panel v-model="panel" expand>
-        <v-expansion-panel-content v-for="(item, index) of playlist" :key="index">
-          <div slot="header">{{item.name}} </div>            
-          <v-card v-for="(track, index) of item.tracks" :key="index" >
-            <Card :name="track.track.name" :image="track.track.album.images['0'].url" :dataL="track.track.album.release_date.substring(0,4)"></Card>
-            <v-card-text
-              class="#303030"
-            ></v-card-text>
-          </v-card>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-
-      
-    </v-flex>
-    <v-btn
-    @click="modalNew = true"
-    fixed
-      dark
-      fab
-      bottom
-      right
-      color="green" >
+  <div>
+    <v-layout fill-height justify-center align-center row wrap>
+      <v-flex xs12 sm12 md12 lg6 xl6>
+        <v-expansion-panel v-model="panel" expand>
+          <v-expansion-panel-content v-for="(item, index) of playlist" :key="index">
+            <div slot="header">{{item.name}}</div>
+            <v-card v-for="(track, index) of item.tracks" :key="index">
+              <Card
+                :name="track.track.name"
+                :image="track.track.album.images['0'].url"
+                :dataL="track.track.album.release_date.substring(0,4)"
+              ></Card>
+              <v-card-text class="#303030"></v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-flex>
+      <v-btn @click="modalNew = true" fixed dark fab bottom right color="green">
         <v-icon>add</v-icon>
       </v-btn>
-
-
-  </v-layout>
-  <v-layout>
-    <v-flex xs12 sm12 md12 lg6 xl6 justify-center align-center row wrap >
-  <v-dialog v-model="modalNew" persistent max-width="80%">
-     <v-layout  align-center row wrap>
-       <v-flex xs12 sm12 md12 lg12 xl12 >
-         <v-text-field class="ml-4 mr-4"  v-model="newPlaylist.name"  label="Name"  required  >
-           
-         </v-text-field>
-         <v-text-field class="ml-4 mr-4" v-model="newPlaylist.description"  label="Description" >
-         </v-text-field>
-       </v-flex>
-   
-       <v-flex class="mb-4" xs12 sm12 md12 lg12 xl12 >
-         <v-btn
-         
-      @click="clearPlaylistForm"
-      color="white"
-      class="black--text ml-4"
-    >cancel
-       </v-btn>
-       <v-btn
-      @click="createPlaylist"
-      color="green"
-
-      
-    >create
-       </v-btn>
-    </v-flex>
-    <v-snackbar
-      v-model="snackbar.snackbar"
-      multi-line="multi-line"
-      right="right"
-      :timeout="snackbar.timeout"
-      top="top"
-      vertical="vertical"
-    >
-    {{snackbar.text}}
-    </v-snackbar>
-     </v-layout>
-    </v-dialog>
-</v-flex>
-  </v-layout>
-</div>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12 sm12 md12 lg6 xl6 justify-center align-center row wrap>
+        <v-dialog v-model="modalNew" persistent max-width="80%">
+          <v-layout align-center row wrap>
+            <v-flex xs12 sm12 md12 lg12 xl12>
+              <v-text-field class="ml-4 mr-4" v-model="newPlaylist.name" label="Name" required></v-text-field>
+              <v-text-field class="ml-4 mr-4" v-model="newPlaylist.description" label="Description"></v-text-field>
+            </v-flex>
+            <v-flex class="mb-4" xs12 sm12 md12 lg12 xl12>
+              <v-btn @click="clearPlaylistForm" color="white" class="black--text ml-4">cancel</v-btn>
+              <v-btn @click="createPlaylist" color="green">create</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-dialog>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 <script>
 import axios from "axios";
 import Card from "~/components/Card";
-//import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
 export default {
   components: {
-    Card,
-   // PulseLoader
+    Card
   },
-
   layout: "home",
 
   data() {
@@ -95,37 +55,26 @@ export default {
       playlist: [],
       musicas: [],
       modalNew: false,
-      newPlaylist:{
+      newPlaylist: {
         name: "",
         collaborative: false,
         description: "",
         public: true
       },
-      snackbar:{
-        snackbar: true,
-        y: 'top',
-        x: null,
-        mode: '',
-        timeout: 6000,
-        text: ''
-      },
+
       loading: false
-      
     };
   },
   created() {
-     if (this.$store.state.user.token === '' || null){
-        this.$router.push('/')
-    }else{
-    this.getPlaylist(this.$store.state.user.token);
-    
+    if (this.$store.state.user.token === "" || null) {
+      this.$router.push("/");
+    } else {
+      this.getPlaylist(this.$store.state.user.token);
     }
-   
-
   },
   methods: {
     getPlaylist(token) {
-      this.loading = true
+      this.loading = true;
       let config = { headers: { Authorization: "Bearer " + token } };
       axios
         .get("https://api.spotify.com/v1/me/playlists", config)
@@ -135,7 +84,6 @@ export default {
           const { items } = res.data;
           await items.map(async item => {
             let t = [];
-            //await this.getTracksPlaylists(item.id, token)
             let a = await {
               id: item.id,
               name: item.name,
@@ -143,13 +91,12 @@ export default {
             };
             this.playlist.push(a);
           });
-           this.loading = false
-           this.showPlaylist()
+          this.loading = false;
+          this.showPlaylist();
         })
         .catch(e => {
-           this.loading = false
-          
-          });
+          this.loading = false;
+        });
     },
     async getTracksPlaylists(id, token) {
       let ms = [];
@@ -168,33 +115,42 @@ export default {
       return ms;
       ms = [];
     },
-    resetForm (){
-this.newPlaylist ={
+    resetForm() {
+      this.newPlaylist = {
         name: "",
         description: "",
         public: true
-      }
+      };
     },
-    clearPlaylistForm(){
-      this.resetForm()
-      this.modalNew = false
+    clearPlaylistForm() {
+      this.resetForm();
+      this.modalNew = false;
     },
-    async createPlaylist (){
-       let config = { headers: { 
-          Authorization: "Bearer " + this.$store.state.user.token } };
-      if (this.newPlaylist.name.length >2){
-       await axios.post(`https://api.spotify.com/v1/users/${this.$store.state.user.id}/playlists`,this.newPlaylist, config).then(async res =>{
-         await this.resetForm()
-         this.playlist =[]
-         await this.getPlaylist(this.$store.state.user.token);
-         this.modalNew = false
-       }).catch(error => console.log(error))
-      
-      }else{
-         console.log( 'preencha os campos name')
+    async createPlaylist() {
+      let config = {
+        headers: {
+          Authorization: "Bearer " + this.$store.state.user.token
+        }
+      };
+      if (this.newPlaylist.name.length > 2) {
+        await axios
+          .post(
+            `https://api.spotify.com/v1/users/${
+              this.$store.state.user.id
+            }/playlists`,
+            this.newPlaylist,
+            config
+          )
+          .then(async res => {
+            await this.resetForm();
+            this.playlist = [];
+            await this.getPlaylist(this.$store.state.user.token);
+            this.modalNew = false;
+          })
+          .catch(error => console.log(error));
+      } else {
+        console.log("preencha os campos name");
       }
-    
-      
     }
   }
 };
